44d5486984783c1f49785675ae8c2b95086c45ec
Peter Emelyanov <piter.emelyanov@gmail.com>
2026-09-01 12:14:19 +0300

    Added FastAPI application skeleton

---
+++ main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from api.handlers import router
+
+app = FastAPI(title="Справочник строительных ресурсов")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
